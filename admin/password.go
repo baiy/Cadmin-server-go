cef0ea3bf28c4310b1f73215eb2d133b702d7fff
baiy <wo@baiy.org>
2019-10-11 18:00:34 +0800

    优化密码生成器加载方式

--- admin/password.go
+++ admin/password.go
@@ -9,19 +9,28 @@ import (
 	"time"
 )
 
-type Passwrod interface {
+// 密码生成器接口
+type Password interface {
 	Hash(password []byte) []byte
 	Verify(password, hash []byte) bool
 }
 
-type PasswrodDefault struct {
+// 注册密码生成器
+func RegisterPassword(password Password) {
+	Passworder = password
 }
 
-func (p *PasswrodDefault) Hash(password []byte) []byte {
+var Passworder Password
+
+// 默认密码生成器
+type PasswordDefault struct {
+}
+
+func (p *PasswordDefault) Hash(password []byte) []byte {
 	return p.hashWithSalt(password, p.salt())
 }
 
-func (p *PasswrodDefault) hashWithSalt(password, salt []byte) []byte {
+func (p *PasswordDefault) hashWithSalt(password, salt []byte) []byte {
 	sha := p.sha265(append(p.sha265(append(password, salt...)), salt...))
 	src := append(
 		sha[:],
@@ -31,7 +40,7 @@ func (p *PasswrodDefault) hashWithSalt(password, salt []byte) []byte {
 	return []byte(base64.StdEncoding.EncodeToString(src))
 }
 
-func (p *PasswrodDefault) Verify(password, hash []byte) bool {
+func (p *PasswordDefault) Verify(password, hash []byte) bool {
 	dst := make([]byte, 0)
 	dst, err := base64.StdEncoding.DecodeString(string(hash))
 	if err != nil {
@@ -44,7 +53,7 @@ func (p *PasswrodDefault) Verify(password, hash []byte) bool {
 	return string(hash) == string(p.hashWithSalt(password, []byte(hashItem[1])))
 }
 
-func (p *PasswrodDefault) salt() []byte {
+func (p *PasswordDefault) salt() []byte {
 	str := "0123456789abcdefghijklmnopqrstuvwxyz"
 	bytes := []byte(str)
 	result := make([]byte, 0)
@@ -55,6 +64,10 @@ func (p *PasswrodDefault) salt() []byte {
 	return result
 }
 
-func (p *PasswrodDefault) sha265(data []byte) []byte {
+func (p *PasswordDefault) sha265(data []byte) []byte {
 	return []byte(fmt.Sprintf("%x", sha256.Sum256(data)))
+}
+
+func init() {
+	RegisterPassword(new(PasswordDefault))
 }
--- admin/password_test.go
+++ admin/password_test.go
@@ -2,8 +2,8 @@ package admin
 
 import "testing"
 
-func TestPasswrod(t *testing.T) {
-	password := new(PasswrodDefault)
+func TestPassword(t *testing.T) {
+	password := new(PasswordDefault)
 
 	result := "YTc1NzQwYzcwYTM3Yzk1NDk5OGNmZDZlMWYyYjcyOWY3NzU5ODdkZDFmNzIwYWI0YTRmNjJiOTM2NDlmYTE5MXw1NzYzNjg1Mw=="
 	if !password.Verify([]byte("123456"), []byte(result)) {
