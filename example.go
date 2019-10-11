24f9f4b2fee43497afc0a55b782dc18d31ffa4ac
baiy <wo@baiy.org>
2019-10-11 18:01:45 +0800

    示例代码更新

--- example.go
+++ example.go
@@ -4,13 +4,15 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/baiy/Cadmin-service-go/admin"
-	_ "github.com/baiy/Cadmin-service-go/system" // 加载Cadmin内置action路由
+	_ "github.com/baiy/Cadmin-service-go/system" // 加载Cadmin内置action
 	_ "github.com/go-sql-driver/mysql"
 	"net/http"
 )
 
 var db *sql.DB
 
+
+// 初始化数据库
 func initDb() {
 	var err error
 	db, err = sql.Open("mysql", "root:root@/admin_api_new?charset=utf8mb4&parseTime=True&loc=Local")
@@ -21,10 +23,29 @@ func initDb() {
 
 func main() {
 	initDb()
-	admin.InjectDb(db)
-	http.HandleFunc("/admin/", func(writer http.ResponseWriter, request *http.Request) {
+
+	// 设置数据库操作对象
+	admin.SetDb(db)
+	// [可选] 注册自定义调度器
+	// admin.RegisterDispatch()
+	// [可选] 设置自定义密码生成器
+	// admin.RegisterPassword()
+	// [可选] 无需校验权限的api
+	// admin.AddNoCheckLoginRequestId()
+	// [可选] 只需登录即可访问的api
+	// admin.AddOnlyLoginRequestId()
+	// [可选] 设置请求标识变量名
+	// admin.ActionName = ""
+	// [可选] 设置请求token变量名
+	// admin.TokenName = ""
+	// [可选] 设置请求日志记录回调函数
+	//admin.RegisterLogCallback()
+
+	http.HandleFunc("/api/admin/", func(writer http.ResponseWriter, request *http.Request) {
+		// 前后端分离项目一般会有跨域问题 自行处理
 		writer.Header().Set("Access-Control-Allow-Origin", "*")
 		writer.Header().Add("Access-Control-Allow-Headers", "Content-Type")
+
 		context := admin.NewContext(writer, request)
 		if err := context.Output(); err != nil {
 			fmt.Println(err)
