f4cbfe6561513b53641915209e6f611d48ee6c4e
baiy <wo@baiy.org>
2019-10-11 19:24:26 +0800

    添加gorm 示例

---
+++ example/gorm/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/baiy/Cadmin-server-go/admin"
+	_ "github.com/baiy/Cadmin-server-go/system" // 注册内置请求处理方法
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"net/http"
+)
+
+func main() {
+	db, _ := gorm.Open("mysql", "root:root@/admin_api_new?charset=utf8mb4&parseTime=True&loc=Local")
+	defer db.Close()
+
+	admin.SetDb(db.DB())
+	// 省略其他配置代码 查看原生示例代码
+	http.HandleFunc("/api/admin/", func(writer http.ResponseWriter, request *http.Request) {
+		// 前后端分离项目一般会有跨域问题 自行处理
+		writer.Header().Add("Access-Control-Allow-Origin", "*")
+		writer.Header().Add("Access-Control-Allow-Headers", "Content-Type")
+
+		context := admin.NewContext(writer, request)
+		if err := context.Output(); err != nil {
+			fmt.Println(err)
+		}
+	})
+	_ = http.ListenAndServe("127.0.0.1:8001", nil)
+
+}
